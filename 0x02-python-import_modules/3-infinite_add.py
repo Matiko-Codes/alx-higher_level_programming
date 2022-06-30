71931f82538f0f9fc2aa379d3fafc8b9af900ef0
Matiko <mmatiko243@gmail.com>
2022-06-30 16:47:16 +0000

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    infinite_sum = 0
+    for i in range(len(sys.argv) - 1):
+        infinite_sum += int(sys.argv[i + 1])
+    print("{}".format(infinite_sum))
