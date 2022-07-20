48b7248c6ed77678b7036ba11bba9fd67e949cbe
Matiko <mmatiko243@gmail.com>
2022-07-20 18:45:14 +0000

    Pycodestyle fix for task 1, 2, 3, 0, 101, 103

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" An empty square class with an attribute"""
+"""And empty square class."""
+
 
 class Square:
-    """An empty square class with an attribute"""
+    """And empty square class with an artribute"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
-""" Module square """
+"""a class Square that defines a square by:
+(based on 0-square.py)
+"""
+
+
 class Square:
-    """Create a rectangle"""
+    """Square class with a private attribute -
+    size.
+    """
+
     def __init__(self, size):
+        """Initializes the size variable as a private
+        instance artribute
+        """
         self.__size = size
