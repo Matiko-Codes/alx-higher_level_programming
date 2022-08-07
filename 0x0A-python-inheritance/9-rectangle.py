4c77dacf8605473d140e0a011f8c0fb0b879129a
Maticodes <98250895+Matiko-Codes@users.noreply.github.com>
2022-08-07 09:30:36 +0300

    8 and 9 fix

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
+"""Rectangle module"""
 
-"""Defines a class Rectangle that inherits
-from the BaseGeometry class.
-"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """A class Rectangle that inherits
-    from the BaseGeometry
-    """
+    """Class Rectangle that inherits from BaseGeometry (7-base_geometry.py)
+    (task based on 8-rectangle.py)"""
 
     def __init__(self, width, height):
-        """Instantiation of width and height"""
+        """Constructor method for Rectangle
+        Instantiation with width and height"""
+
         super().integer_validator("width", width)
-        self.__width = width
         super().integer_validator("height", height)
+        self.__width = width
         self.__height = height
 
     def area(self):
-        """Returns the area of the rectangle"""
-        return self.__width * self.__height
+        """Return area of rectangle"""
+
+        area = self.__width * self.__height
+        return area
 
     def __str__(self):
-        """Return the print() and str()
-        representation of a Rectangle.
-        """
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__width) + "/" + str(self.__height)
+        """Returns string representation of a Rectangle"""
+
+        string = "[Rectangle] {}/{}".format(self.__width, self.__height)
         return string
