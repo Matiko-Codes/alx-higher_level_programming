b4c8be6fcde805615318c7e64ab28df908339f39
Matiko-Codes <mmatiko243@gmail.com>
2022-09-06 16:16:10 +0000

    Remaining tasks

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Script that remove all scores minor than  5 --
+DELETE FROM second_table WHERE (score <= 5);
+
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- finding the score in the second table --
+SELECT AVG(score) as average FROM second_table;
+
