270be1b689e03cf100f403e048c4a694ae4f704b
Matiko-Codes <mmatiko243@gmail.com>
2022-10-22 08:10:56 +0000

    Fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,17 +10,5 @@ if __name__ == "__main__":
         print('Body response:')
         print('\t- type:', type(html))
         print('\t- content:', html)
-        print('\t- utf8 content:', html.decode('UTF-8'))#!/usr/bin/python3
-"""urllib usage"""
-import urllib.request
-import urllib.parse
-
-
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type:', type(html))
-        print('\t- content:', html)
         print('\t- utf8 content:', html.decode('UTF-8'))
 
